fc616c74c92113dc1ed2287906f48ff5ad794b0a
raxor <jannik.markau01@gmail.com>
2021-12-21 20:20:17 +0100

    Anfänlgliche Fach einarbeitung

---
+++ src/main/java/com/example/helpstudent/Config/FachConfig.java
@@ -0,0 +1,95 @@
+package com.example.helpstudent.Config;
+
+import com.example.helpstudent.Repository.FachRepository;
+import com.example.helpstudent.Repository.GruppenRepository;
+import com.example.helpstudent.Tabellen.Student.Fach;
+import com.example.helpstudent.Tabellen.Student.Gruppe;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FachConfig {
+    @Bean
+    CommandLineRunner commandLineRunner4(FachRepository repository){
+        return args -> {
+            repository.save(new Fach("Arbeitstechniken u. Selbstmanagement",1));
+            repository.save(new Fach("Schriftl. Kommunikation u. wissenschaftl.",1));
+            repository.save(new Fach("Programmieren 1 VL",1));
+            repository.save(new Fach("Webtechnologien VL",1));
+            repository.save(new Fach("Webtechnologien ÜB ",1));
+            repository.save(new Fach("Datenbanken VL",1));
+            repository.save(new Fach("Grundlagen der Sozialen Medien VL",1));
+            repository.save(new Fach("Mathematik 1 VL",1));
+            repository.save(new Fach("Grundlagen der Sozialen Medien SE ",1));
+            repository.save(new Fach("Mathematik 1 ÜB",1));
+            repository.save(new Fach("Datenbanken ÜB",1));
+            repository.save(new Fach("Programmieren 1 PR",1));
+            repository.save(new Fach("Programmieren 1 VL",1));
+
+            repository.save(new Fach("Mathematik 3 VL",3));
+            repository.save(new Fach("Mathematik 3 ÜB",3));
+            repository.save(new Fach("Softwareprojekt 1 PR",3));
+            repository.save(new Fach("Analyse in sozialen Netzwerken VL",3));
+            repository.save(new Fach("Computersysteme VL",3));
+            repository.save(new Fach("Analyse in sozialen Netzwerken ÜB ",3));
+            repository.save(new Fach("Computersysteme ÜB",3));
+            repository.save(new Fach("Softwaretechnik 1 VL",3));
+            repository.save(new Fach("Softwaretechnik 1 ÜB",3));
+            repository.save(new Fach("Mathematik 1 ÜB",3));
+            repository.save(new Fach("Programmierkonzepte VL",3));
+            repository.save(new Fach("Programmierkonzepte ÜB",3));
+            repository.save(new Fach("Projektmanagement 1 SE",3));
+
+            repository.save(new Fach("IT-Sicherheitsmanagement SE",7));
+            repository.save(new Fach("Webapplikationssicherheit SE",7));
+            repository.save(new Fach("Recommender Systems SE",7));
+            repository.save(new Fach("Big Data and Cloud Computing SE",7));
+            repository.save(new Fach("Informationsvisualisierung SE",7));
+            repository.save(new Fach("Gestenbasierte Systeme SE",7));
+
+
+
+            repository.save(new Fach("Unternehmensführung VL",1));
+            repository.save(new Fach("Kundenmanagement",1));
+            repository.save(new Fach("Unternehmensführung ÜB",1));
+            repository.save(new Fach("Marktforschung VL/ÜB ",1));
+            repository.save(new Fach("Sustainability Management VL/ÜB",1));
+            repository.save(new Fach("Sustainability Management SE",1));
+            repository.save(new Fach("Marketing SE",1));
+            repository.save(new Fach("Unternehmensführung SE",1));
+
+            repository.save(new Fach("Finance SE",3));
+            repository.save(new Fach("Personalmanagement SE",3));
+            repository.save(new Fach("Personalmanagement VL/ÜB",3));
+            repository.save(new Fach("Externes Rechnungswesen SE",3));
+            repository.save(new Fach("Finance VL/ÜB",3));
+            repository.save(new Fach("Externes Rechnungswesen",3));
+
+
+
+            repository.save(new Fach("Marketing VL",1));
+            repository.save(new Fach("VWL VL/ÜB",1));
+            repository.save(new Fach("Englisch VL/ÜB",1));
+            repository.save(new Fach("Englisch VL/ÜB ",1));
+            repository.save(new Fach("Rechnungswesen VL",1));
+            repository.save(new Fach("Beschaffung und Produktion VL",1));
+            repository.save(new Fach("Steuern VL ",1));
+
+            repository.save(new Fach("Konsumentenverhalten VL",3));
+            repository.save(new Fach("Konsumentenverhalten ÜB",3));
+            repository.save(new Fach("Verhalten in Organisationen ÜB",3));
+            repository.save(new Fach("Konsumentenverhalten SE",3));
+            repository.save(new Fach("Verhalten in Organisationen SE",3));
+            repository.save(new Fach("Arbeitstechniken u. Selbstmanagement SE",3));
+            repository.save(new Fach("International Economics SE",3));
+            repository.save(new Fach("Wirtschaftspolitik ÜB",3));
+            repository.save(new Fach("Wirtschaftspolitik VL",3));
+            repository.save(new Fach("Wirtschaftspolitik SE",3));
+            repository.save(new Fach("Controlling SE",3));
+            repository.save(new Fach("Strategisches Management SE ",3));
+            repository.save(new Fach("Unternehmensbesteuerung SE",3));
+
+        };
+    }
+}
---
+++ src/main/java/com/example/helpstudent/Repository/FachRepository.java
@@ -0,0 +1,8 @@
+package com.example.helpstudent.Repository;
+
+import com.example.helpstudent.Tabellen.Student.Fach;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FachRepository extends JpaRepository<Fach, Long> {
+
+}
