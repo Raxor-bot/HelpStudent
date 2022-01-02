b4ca9fe6cfdcfef1f854aadbd13a711a8476b109
raxor <jannik.markau01@gmail.com>
2022-01-02 18:00:22 +0100

    Filecontroller umbenant

--- src/main/java/com/example/helpstudent/Controller/UpdateController.java
+++ src/main/java/com/example/helpstudent/Controller/UpdateController.java
@@ -3,34 +3,35 @@ package com.example.helpstudent.Controller;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Optional;
 
 import com.example.helpstudent.Service.StudentService;
 import com.example.helpstudent.Tabellen.Student.Student;
+
 import org.hibernate.annotations.common.util.impl.LoggerFactory;
 import org.jboss.logging.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
-public class FileController {
-    private final Logger logger = LoggerFactory.logger(FileController.class);
+public class UpdateController {
+    private final Logger logger = LoggerFactory.logger(UpdateController.class);
     private final StudentService studentService;
 
-    public FileController(StudentService studentService) {
+    public UpdateController(StudentService studentService) {
         this.studentService = studentService;
     }
 
-    @RequestMapping(value = "/upload" , consumes = {"multipart/form-data"})
-    public ResponseEntity<?> handleFilesUpload(@RequestParam("file") MultipartFile file,@RequestParam("nfdlStudent") long id, Model map) throws IOException { //Hier noch StudentID
+    @RequestMapping(value = "/upload")
+    public ResponseEntity<?> handleFilesUpload(@RequestParam("file") MultipartFile file,@RequestParam("id") long id, Model map) throws IOException { //Hier noch StudentID
         StringBuilder sb = new StringBuilder();
         Optional<Student> student = studentService.getStudentByID(id);
 
