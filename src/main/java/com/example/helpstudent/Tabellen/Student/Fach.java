9436c6e1751bf6bb89789efec5ad0b0ba0bc102e
kleinerbott <kleinerbott@gmail.com>
2022-01-06 12:23:40 +0100

    FachFix (Getter und Setter hinzugefügt)

--- src/main/java/com/example/helpstudent/Tabellen/Student/Fach.java
+++ src/main/java/com/example/helpstudent/Tabellen/Student/Fach.java
@@ -1,20 +1,24 @@
 package com.example.helpstudent.Tabellen.Student;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
-import java.util.*;
 
 @Entity
+@Getter
+@Setter
 @Table(name="fach")
 public class Fach {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private long nlfdFach;
     private String fachName;
     private int semester;
+
    // @ManyToMany(mappedBy = "faecher")
     //private List<Studiengang> stdList;
-
     public Fach(String fachName, int semester) {
         this.fachName = fachName;
         this.semester = semester;
